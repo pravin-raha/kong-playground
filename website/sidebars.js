660f450a1f86ec1f4ba81ded24951c58aeda5da5
pravin-raha <pravin-raha@users.noreply.github.com>
2022-08-23 17:25:48 +0000

    Deploy website - based on 1f22b5f1b7faa58afcf31445f19a4fc5f4a9182d

--- website/sidebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  someSidebar: {
-    'Environment Setup': ['getting_started', 'service_list', 'route_and_service',
-                          'rate_limit', 'authentication_plugin','load_balancer'],
-  },
-};
